696e9206fa8e73a1f95f6483426747811f0314cb
Paul Johnson <paul@snuberry-software.co.uk>
2018-08-14 23:38:34 +0100

    Initial import

---
+++ TwoPageSwitch/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TwoPageSwitch
+{
+    public partial class MainPage : ContentPage
+    {
+        bool Toggled { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        void Handle_Toggled(object sender, ToggledEventArgs e)
+        {
+            Toggled = e.Value;
+        }
+
+        void Handle_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageTwo(Toggled));
+        }
+    }
+}
---
+++ TwoPageSwitch/PageTwo.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace TwoPageSwitch
+{
+    public partial class PageTwo : ContentPage
+    {
+        public PageTwo()
+        {
+            InitializeComponent();
+        }
+
+        public PageTwo(bool toggled)
+        {
+            InitializeComponent();
+            imgToggled.Source = toggled ? "image1" : "image2";
+        }
+    }
+}
